49646666ed03b004508f1eccc7f02bb3d97bff7e
ravindrabijore <135633718+ravindrabijore@users.noreply.github.com>
2024-02-06 16:12:59 +0530

    Create CounterInView.js

---
+++ CounterInView.js
@@ -0,0 +1,57 @@
+import React, { useState, useEffect, useRef } from 'react'; 
+
+const CounterInView = (props) => {
+  const [isVisible, setIsVisible] = useState(false);
+  const sectionRef = useRef(null);
+
+  const [count, setCount] = useState(0);
+  const counterRef = useRef(null);
+
+  let start = props.start;
+  const end = props.end; // Set your desired end value here
+  const duration = props.duration; // Set animation duration in milliseconds
+  let totalSteps = duration / 16; // Number of steps (assuming 60fps)
+
+
+  const incrementCount = () => {
+    const increment = end / totalSteps;
+    if (start < end) {
+      setCount((prevCount) => prevCount + increment);
+      start += increment;
+      requestAnimationFrame(incrementCount);
+    } else {
+      setCount(end);
+    }
+  };
+ 
+  useEffect(() => {
+    const observer = new IntersectionObserver((entries) => {
+      entries.forEach((entry) => {
+        if (entry.isIntersecting) {
+          setIsVisible(true);
+          observer.unobserve(entry.target);
+        }
+      });
+    }, { threshold: 1 }); // Trigger when at least 50% of the target is visible
+
+    if (sectionRef.current) {
+      observer.observe(sectionRef.current);
+    }
+    if (isVisible) {
+        incrementCount();
+      }
+
+  }, [isVisible]);
+
+  return (
+    <section ref={sectionRef}>
+      { isVisible && (
+        <div  style={{textAlign:"center"}}  ref={counterRef} >
+            <h2  >{Math.floor(count)}</h2>
+            <h2  >Projects</h2>
+        </div>) }
+    </section>
+  );
+};
+
+export default CounterInView;
